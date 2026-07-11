6c87b5fb8234db433ee763021634ff14c205fde4
guanhai <q2274799107@126.com>
2026-07-11 19:57:55 +0800

    feat(internal/config): 使config配置能够区分环境编译产物,并且每次编译后都会清理之前的产物

--- internal/config/build/index.js
+++ internal/config/build/index.js
@@ -1,35 +1,62 @@
 import * as path from "node:path";
+import process from "node:process";
 import { fileURLToPath } from "node:url";
+import { toMerged } from "es-toolkit";
+
 import * as esbuild from "esbuild";
+import { clean } from "esbuild-plugin-clean";
+import { environmentPlugin } from "esbuild-plugin-environment";
 
 const rootPath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "../src");
+const env = process.env.NODE_ENV;
 
-// 构建
-// eslint-disable-next-line antfu/no-top-level-await
-const ctx = await esbuild.context({
+const conf = {
   entryPoints: [
     { in: path.resolve(rootPath, "tools/index.ts"), out: "tools" },
     { in: path.resolve(rootPath, "main.ts"), out: "webpack" },
   ],
   bundle: true,
   platform: "node",
-  outdir: "target",
+  outdir: "dist",
   // 将 esbuild 生成的文件的文件扩展名自定义
   outExtension: { ".js": ".mjs" },
   format: "esm",
   // 标记为 外部，不参与打包
   external: ["mini-css-extract-plugin", "fork-ts-checker-webpack-plugin"],
-  sourcemap: "external",
-  minify: false,
   treeShaking: true,
   allowOverwrite: true,
   alias: {
     "#": rootPath,
   },
   resolveExtensions: [".ts"],
-  plugins: [],
-});
+  plugins: [
+    // 提供环境变量
+    environmentPlugin(["NODE_ENV"]),
+    // 每次构建清理之前的构建产物
+    clean({
+      patterns: ["./dist/*"],
+    }),
+  ],
+};
 
-// 监听
-// eslint-disable-next-line antfu/no-top-level-await
-await ctx.watch();
+if (env === "dev") {
+  // 监听
+  (async () => {
+    const config = toMerged(conf, {
+      sourcemap: "external",
+      minify: false,
+    });
+    const ctx = await esbuild.context(config);
+    await ctx.watch();
+  })();
+} else if (env === "prod") {
+  (async () => {
+    const config = toMerged(conf, {
+      sourcemap: false,
+      minify: true,
+    });
+    const ctx = await esbuild.context(config);
+    await ctx.rebuild();
+    ctx.dispose();
+  })();
+}
